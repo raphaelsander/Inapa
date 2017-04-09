bd311e165a27e53d917d8fba9cb426dcb2fbf0e6
Raphael Sander <raphael.sander75@gmail.com>
2017-04-09 17:51:15 -0300

    * Inserido cabeçalho de versão e coding(utf-8); * Inserido animação ao GIF loading; * Definido código do cursos de acordo com o dropdown; * Criação da função de login, funcionando com bugs para solucionar(vide arquivo bug); * Login para todos os cursos funcionando.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python2.7
+#-*- coding: utf-8 -*-
+
 import sys
 from PyQt4 import QtGui
 from PyQt4.uic import loadUiType
@@ -8,8 +11,13 @@ Ui_LoginWindow, QLoginWindow = loadUiType('ui/login.ui')
 
 class Login(QLoginWindow, Ui_LoginWindow):
     def __init__(self):
-        super(Login, self).__init__()
+        super(Login, self).__init__(None)
         self.setupUi(self)
+
+        self.label_4.hide()
+        self.gif_loading = QtGui.QMovie("ui/loading.gif")
+        self.label_4.setMovie(self.gif_loading)
+        self.gif_loading.start()
 
     def on_lineEdit_textEdited(self, text):
         self.matr = text
@@ -17,17 +25,45 @@ class Login(QLoginWindow, Ui_LoginWindow):
     def on_lineEdit_2_textEdited(self, text):
         self.senha = text
 
-    def on_comboBox_activated(self, QString):
-        self.curso = QString
+    def on_comboBox_currentIndexChanged(self, int):
+        if int == 0:
+            self.curso = "1"
+        if int == 1:
+            self.curso = "2"
+        if int == 2:
+            self.curso = "12"
+        if int == 3:
+            self.curso = "6"
+        if int == 4:
+            self.curso = "24"
+        if int == 5:
+            self.curso = "5"
+        if int == 6:
+            self.curso = "7"
+        if int == 7:
+            self.curso = "21"
+        if int == 8:
+            self.curso = "31"
+
+    def on_pushButton_pressed(self):
+        hey = Main(self.matr, self.senha, self.curso)
+        hey.provas()
+        hey.show()
+        login.hide()
+        return app.exec_()
 
 class Main(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super(Main, self).__init__()
+    def __init__(self, matr, senha, curso, parent=None):
+        super(Main, self).__init__(parent)
         self.setupUi(self)
+
+        self.matr = matr
+        self.senha = senha
+        self.curso = curso
 
     def provas(self):
 
-        lista_provas = get_provas("000", "000000", "21")
+        lista_provas = get_provas(self.matr, self.senha, self.curso)
 
         num_itens = len(lista_provas)/7
         self.tableWidget.setRowCount(num_itens)
@@ -44,9 +80,10 @@ class Main(QMainWindow, Ui_MainWindow):
             if cont == 7:
                 cont2 = cont2 + 1
                 cont = 0
+        self.tableWidget.update()
 
 if __name__ == '__main__' :
 	app = QtGui.QApplication(sys.argv)
-	main = Main()
-	main.show()
-	sys.exit(app.exec_())
+	login = Login()
+	login.show()
+	app.exec_()
